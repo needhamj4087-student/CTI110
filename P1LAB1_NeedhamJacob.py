3eae7af1c6f87b079d81c627d8c0605c185ccf7f
needhamj4087-student <needhamj4087@student.faytechcc.edu>
2026-06-10 17:53:41 -0400

    first Python assignment
    taking user input and printing output

---
+++ P1LAB1_NeedhamJacob.py
@@ -0,0 +1,16 @@
+#Jacob Needham
+#06/09/2026
+#P1LAB1_NeedhamJacob.py
+#Discription: Python script that takes User input for  first and last name and it welcomes them to class.
+
+firstName = input(
+    "Enter your first name: "
+)
+
+lastName = input(
+    "Enter your last name: "
+)
+
+print(
+    "Hello,", firstName, lastName +"!",  "Welcome to CTI-110!"
+)
