e7387ad0951a55449d237d78334889cf73ff4226
Jacob <needhamj4087.student.faytechcc.edu>
2026-06-11 11:00:52 -0400

    added exponent function to p1hw1

--- P1HW1_NeedhamJacob.py
+++ P1HW1_NeedhamJacob.py
@@ -1,3 +1,20 @@
 #Jacob Needham
 #06/10/2026
 #P1HW1.py
+#In tis program we will calculate exponenets and some other addition and subtraction functions
+
+#Calculate Exponents
+
+print(
+    "--------Exponents--------"
+)
+print()
+
+base = int(input("Enter a base number: "))
+exponent = int(input("Enter an exponent: "))
+
+result = base ** exponent
+
+print(
+    base, "raised to the power of", exponent, "is", result
+)
