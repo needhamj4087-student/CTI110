07334a174a97a48b9662f8c3e09b247a21c6eca7
Jacob <needhamj4087.student.faytechcc.edu>
2026-07-04 16:29:03 -0400

    updated P4HW1

--- P4HW1_NeedhamJacob.py
+++ P4HW1_NeedhamJacob.py
@@ -10,7 +10,79 @@
 #Hint - you will need to use more than one loop in this program
 #
 #
+#Create and interpret Lists of input data, module grades.
 
+#User input grades for Module 1 thru 6 one by one
+scores = int(input(
+    "How Many Scores do you wish to enter? "
+))
+#list for user scores
+userScores = []
+print()
+
+#loop to collect the users scores
+for num in range(1, scores + 1):
+    while True:
+        score = int(input(f"Enter Score #{num}: "))
+        if 0 <= score <= 100:
+            userScores.append(score)
+            break
+        else:
+            print()
+            print("INVALID Score entered!!!")
+            print("Score should be between 0 and 100")
+            print(f"Enter score #{num} again: ")
+            
+
+print()
+
+print(
+    "----------Results----------"
+)
+
+#Display the Lowest Grade
+
+
+print(
+ f"{'Lowest Score':<20}:{min(userScores):.1f}"   
+)
+
+# Remove the lowest Score from the list then display it
+userScores.remove(min(userScores))
+
+print(
+    f"{'Modified List':<20}:{userScores}"
+)
+
+
+
+
+
+
+#Display GPA
+Avg = sum(userScores) / len(userScores)
+print(
+    f"{'Scores Average:':<20}:{Avg:.2f}"
+)
+
+# find the letter grade
+if Avg >= 90:
+    letter_grade = 'A'
+elif Avg >= 80:
+    letter_grade = 'B'
+elif Avg >= 70:
+    letter_grade = 'C'
+elif Avg >= 60:
+    letter_grade = 'D'
+else:
+    letter_grade = 'F'
+
+print(f"{'Grade':<20}:{letter_grade}")
+
+    
+print(
+    "---------------------------"
+)
 
 
 
