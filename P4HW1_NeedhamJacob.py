d09c4b246e21defa272c7801c4184386ad43d433
Jacob <needhamj4087.student.faytechcc.edu>
2026-07-04 12:27:45 -0400

    uddate P4HW1 description

--- P4HW1_NeedhamJacob.py
+++ P4HW1_NeedhamJacob.py
@@ -1,7 +1,13 @@
 #Jacob Needham
 #06//2026
 #P4HW1
-#
+#A Program to 
+#Ask user to enter for number of scores they would like to enter. (10 points)
+#Create a loop to collect the number of scores the user wants to enter. (25 points)
+#Note every time a score is entered, the following should be done
+#Evaluate if the score is valid, it should be between 0 and 100 . 
+#If it is not, notify the user and ask for a VALID score to be entered. (20 points)
+#Hint - you will need to use more than one loop in this program
 #
 #
 
